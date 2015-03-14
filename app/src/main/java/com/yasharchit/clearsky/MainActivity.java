758481a62309276aa69ef503176c8369ab45d745
Yash Archit <archit.yash@gmail.com>
2015-03-14 12:43:33 +0530

    changed mtv zip to lko

--- app/src/main/java/com/yasharchit/clearsky/MainActivity.java
+++ app/src/main/java/com/yasharchit/clearsky/MainActivity.java
@@ -119,7 +119,7 @@ public class MainActivity extends ActionBarActivity {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=226010&mode=json&units=metric&cnt=7");
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
